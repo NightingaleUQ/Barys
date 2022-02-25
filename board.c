a7db7c64d273e19b7d57550e686d143393cc5396
Nightingale <r.nightingale@uq.edu.au>
2022-02-25 18:44:01 +1000

    Wrote the movement rules for all other pieces.

--- board.c
+++ board.c
@@ -54,14 +54,14 @@ void from_0x88(uint8_t pos, uint8_t* rank, uint8_t* file) {
 
 static const struct State initialState = {
     .board = {
-        WHITE | ROOK, WHITE | KNIGHT, WHITE | BISHOP, WHITE | QUEEN, WHITE | KING, WHITE | BISHOP, WHITE | KNIGHT, WHITE | ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
-        WHITE | PAWN, WHITE | PAWN, WHITE | PAWN, WHITE | PAWN, WHITE | PAWN, WHITE | PAWN, WHITE | PAWN, WHITE | PAWN,  0, 0, 0, 0, 0, 0, 0, 0,
+        WHITE|ROOK, WHITE|KNIGHT, WHITE|BISHOP, WHITE|QUEEN, WHITE|KING, WHITE|BISHOP, WHITE|KNIGHT, WHITE|ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
+        WHITE|PAWN, WHITE|PAWN, WHITE|PAWN, WHITE|PAWN, WHITE|PAWN, WHITE|PAWN, WHITE|PAWN, WHITE|PAWN,  0, 0, 0, 0, 0, 0, 0, 0,
         NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE,  0, 0, 0, 0, 0, 0, 0, 0,
         NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE,  0, 0, 0, 0, 0, 0, 0, 0,
         NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE,  0, 0, 0, 0, 0, 0, 0, 0,
         NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE,  0, 0, 0, 0, 0, 0, 0, 0,
-        BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN,  0, 0, 0, 0, 0, 0, 0, 0,
-        BLACK | ROOK, BLACK | KNIGHT, BLACK | BISHOP, BLACK | QUEEN, BLACK | KING, BLACK | BISHOP, BLACK | KNIGHT, BLACK | ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
+        BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN,  0, 0, 0, 0, 0, 0, 0, 0,
+        BLACK|ROOK, BLACK|KNIGHT, BLACK|BISHOP, BLACK|QUEEN, BLACK|KING, BLACK|BISHOP, BLACK|KNIGHT, BLACK|ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
     },
     .ply = 0,
     .last = NULL,
@@ -72,14 +72,14 @@ static const struct State initialState = {
 
 static const struct State whiteTest = {
     .board = {
-        WHITE | ROOK, WHITE | KNIGHT, WHITE | BISHOP, WHITE | QUEEN, WHITE | KING, WHITE | BISHOP, WHITE | KNIGHT, WHITE | ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
+        WHITE|ROOK, WHITE|KNIGHT, WHITE|BISHOP, WHITE|QUEEN, WHITE|KING, WHITE|BISHOP, WHITE|KNIGHT, WHITE|ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE,  0, 0, 0, 0, 0, 0, 0, 0,
         NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE,  0, 0, 0, 0, 0, 0, 0, 0,
         NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE,  0, 0, 0, 0, 0, 0, 0, 0,
         NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE, NO_PIECE,  0, 0, 0, 0, 0, 0, 0, 0,
-        BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN, BLACK | PAWN,  0, 0, 0, 0, 0, 0, 0, 0,
-        BLACK | ROOK, BLACK | KNIGHT, BLACK | BISHOP, BLACK | QUEEN, BLACK | KING, BLACK | BISHOP, BLACK | KNIGHT, BLACK | ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
+        BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN, BLACK|PAWN,  0, 0, 0, 0, 0, 0, 0, 0,
+        BLACK|ROOK, BLACK|KNIGHT, BLACK|BISHOP, BLACK|QUEEN, BLACK|KING, BLACK|BISHOP, BLACK|KNIGHT, BLACK|ROOK, 0, 0, 0, 0, 0, 0, 0, 0,
     },
     .ply = 0,
     .last = NULL,
@@ -89,7 +89,7 @@ static const struct State whiteTest = {
 };
 
 // Allocates memory for a successor state and pre-populates some fields.
-static struct State* add_result(struct State* s, uint8_t orig, uint8_t tgt) {
+static struct State* add_result(struct State* s, struct Move* m) {
     if (s->succ == NULL) {
         s->succ = malloc(48 * sizeof(struct State));
         s->cSucc = 48;
@@ -98,32 +98,39 @@ static struct State* add_result(struct State* s, uint8_t orig, uint8_t tgt) {
         s->succ = realloc(s->succ, s->cSucc);
     }
 
+    // Copy state
     struct State* suc = &s->succ[s->nSucc];
     memcpy(suc, s, sizeof(struct State));
 
-    // Record whether a piece has been moved before (for castling)
-    suc->board[tgt] = s->board[orig] | PIECE_MOVED;
-    suc->board[orig] = 0;
+    // Move piece
+    // Also record whether a piece has been moved before (for castling)
+    suc->board[m->dest] = s->board[m->orig] | PIECE_MOVED;
+    suc->board[m->orig] = 0;
 
+    // Update move number and information on last move
     suc->ply++;
     suc->last = s;
+    memcpy(&suc->lastMove, m, sizeof(struct Move));
     return &s->succ[(s->nSucc)++];
 }
 
-static void move_piece(struct State* s, struct Move* m) {
+// Adds states to successor if the piece can move there.
+static struct State* move_piece(struct State* s, struct Move* m) {
     m->pieceCaptured = 0;
     m->valid = 0;
 
     if (!is_on_board(m->dest)) {
-        return;
+        return NULL;
     }
-    uint8_t piece = s->board[m->dest];
-    if (IS_VACANT(piece) || ((BLACK_TO_MOVE(s) && IS_WHITE(piece)) || (WHITE_TO_MOVE(s) && IS_BLACK(piece)))) {
+    uint8_t tgt = s->board[m->dest];
+    if (IS_VACANT(tgt) || ((BLACK_TO_MOVE(s) && IS_WHITE(tgt)) || (WHITE_TO_MOVE(s) && IS_BLACK(tgt)))) {
         // Move piece to either a vacant square or capture.
-        struct State* succ = add_result(s, m->orig, m->dest);
+        struct State* succ = add_result(s, m);
         m->pieceCaptured = !IS_VACANT(s->board[m->dest]);
         m->valid = 1;
+        return succ;
     }
+    return NULL;
 }
 
 static void slide_piece(struct State* s, uint8_t orig, int8_t dirn, uint8_t isKing) {
@@ -139,17 +146,35 @@ static void slide_piece(struct State* s, uint8_t orig, int8_t dirn, uint8_t isKi
     }
 }
 
+// Checks a state for a pawn that is due to be promoted.
+static void check_promotion(struct State* s, const struct Move *m) {
+    if (s == NULL) return;
+    uint8_t destRank, destFile;
+    from_0x88(m->dest, &destRank, &destFile);
+    if ((BLACK_TO_MOVE(s) && destRank == 0) || (WHITE_TO_MOVE(s) && destRank == 7)) {
+        // Promote this state to Queen TODO Other promotions
+        s->board[m->dest] &= PIECE(0xFF);
+        s->board[m->dest] |= QUEEN;
+    }
+}
+
+// Rook: 0-3, Bishop: 4-7, Queen and King: 0-7;
 static const int8_t slideDirns[8] = {LEFT, RIGHT, UP, DOWN, UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT};
 static const int8_t knightDirns[8] = {UP+UP_LEFT, UP+UP_RIGHT, RIGHT+UP_RIGHT, RIGHT+DOWN_RIGHT, DOWN+DOWN_LEFT, DOWN+DOWN_RIGHT, LEFT+DOWN_LEFT, LEFT+UP_LEFT};
+// Forward: 0, Two-step: 1, Capture: 2-3, En passant: 4-5 (corresponding to 2-3, respectively)
+static const int8_t pawnDirnsBlack[6] = {DOWN, DOWN+DOWN, DOWN_LEFT, DOWN_RIGHT, LEFT, RIGHT};
+static const int8_t pawnDirnsWhite[6] = {UP, UP+UP, UP_LEFT, UP_RIGHT, LEFT, RIGHT};
 
 void get_legal_moves(struct State* s) {
-    struct State* stateNext;
+    struct State* succ;
     for (int8_t r = 0; r < 8; r++)
     for (int8_t f = 0; f < 8; f++) {
         uint8_t orig = to_0x88(r, f);
-        // CHECK COLOUR
-        if ((BLACK_TO_MOVE(s) && IS_WHITE(s->board[orig])) || (WHITE_TO_MOVE(s) && IS_BLACK(s->board[orig]))) continue;
-        uint8_t piece = PIECE(s->board[orig]);
+        // CHECK COLOUR and then extract only piece type.
+        uint8_t piece = s->board[orig];
+        if ((BLACK_TO_MOVE(s) && IS_WHITE(piece)) || (WHITE_TO_MOVE(s) && IS_BLACK(piece))) continue;
+        piece = PIECE(piece);
+
         // ROOK
         if (piece == ROOK) {
             for (int8_t dirn = 0; dirn <= 3; dirn++) {
@@ -173,15 +198,67 @@ void get_legal_moves(struct State* s) {
             for (int8_t dirn = 0; dirn <= 7; dirn++) {
                 slide_piece(s, orig, slideDirns[dirn], 1);
             }
+            // TODO Castling
         }
         // KNIGHT
         if (piece == KNIGHT) {
             for (int8_t dirn = 0; dirn <= 7; dirn++) {
-                // Try each of the squares
+                uint8_t dest = orig + knightDirns[dirn];
+                struct Move m = {.orig = orig, .dest = dest};
+                move_piece(s, &m);
             }
         }
+        // PAWN
+        if (piece == PAWN) {
+            // Clear the two-step flag from the last move
+            if ((BLACK_TO_MOVE(s) && IS_BLACK(piece)) || (WHITE_TO_MOVE(s) && IS_WHITE(piece))) {
+                s->board[orig] &= ~PAWN_TWO_STEP;
+            }
 
-        // TODO Knight, Pawn (two-step, promotion), En passant, Castling
+            struct Move m;
+            m.orig = orig;
+            const int8_t* pawnDirns = BLACK_TO_MOVE(s) ? pawnDirnsBlack : pawnDirnsWhite;
+
+            // Square in front is clear: move forward one or two ranks.
+            m.dest = orig + pawnDirns[0];
+            if (IS_VACANT(s->board[m.dest])) {
+                succ = move_piece(s, &m);
+                check_promotion(succ, &m);
+
+                // Two-step
+                m.dest = orig + pawnDirns[1];
+                if (IS_VACANT(s->board[m.dest]))
+                if ((BLACK_TO_MOVE(s) && r == 6) || (WHITE_TO_MOVE(s) && r == 1)) {
+                    succ = move_piece(s, &m);
+                    if (succ != NULL) {
+                        // Record two-step
+                        succ->board[m.dest] |= PAWN_TWO_STEP;
+                        check_promotion(succ, &m);
+                    }
+                }
+            }
+
+            for (uint8_t i = 2; i <= 3; i++) {
+                // Capture: Square along diagonal contains a piece of opposite colour.
+                m.dest = orig + pawnDirns[i];
+                uint8_t tgt = s->board[m.dest];
+                if ((BLACK_TO_MOVE(s) && IS_WHITE(tgt)) || (WHITE_TO_MOVE(s) && IS_BLACK(tgt))) {
+                    succ = move_piece(s, &m);
+                    check_promotion(succ, &m);
+                }
+
+                // En passant: Check rank and pieces beside and clear destination.
+                // m.dest is the same value used for normal capture above.
+                uint8_t adjacent = s->board[orig + pawnDirns[i + 2]];
+                if ((BLACK_TO_MOVE(s) && r == 3 && IS_VACANT(tgt) && IS_WHITE(adjacent) && PIECE(adjacent) == PAWN && IS_PAWN_TWO_STEP(adjacent)) ||
+                        (WHITE_TO_MOVE(s) && r == 4 && IS_VACANT(tgt) && IS_BLACK(adjacent) && PIECE(adjacent) == PAWN && IS_PAWN_TWO_STEP(adjacent))) {
+                    succ = move_piece(s, &m);
+                    // Additionally, remove en passant-ed piece
+                    if (succ != NULL)
+                        succ->board[adjacent] = NO_PIECE;
+                }
+            }
+        }
     }
     // TODO Remove entries that are in check
 }
